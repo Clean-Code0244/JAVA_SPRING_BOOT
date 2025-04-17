ff65bc1a3109ea50bd1f8a99c6d8445436359e14
Clean-Code0244 <tursunyusufomer@gmail.com>
2025-04-17 19:15:04 +0300

    BCryption applied

--- 05-spring-boot-rest-security/04-spring-boot-rest-security-employee-starter-BCrypt-Encryption/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/04-spring-boot-rest-security-employee-starter-BCrypt-Encryption/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +17,13 @@ public class DemoSecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource datasource){
-        return new JdbcUserDetailsManager(datasource);
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(datasource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_name from employee where id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select role from employee_roles where employee_id=?");
+
+        return jdbcUserDetailsManager;
     }
 
 
